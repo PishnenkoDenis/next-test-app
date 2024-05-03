9926c33ff82f51cb1de033ad2a5ed7d28f3c53dd
d.pishnenko <d.pishnenko@tg.dunice.net>
2024-05-03 17:37:41 +0300

    Add Auth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,6 +4,8 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
+import { signIn } from '../auth';
+import { AuthError } from 'next-auth';
 
 type TStringNull = string | null;
 
@@ -146,4 +148,24 @@ export async function createUpdateInvoice(
   }
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
+}
+
+export async function authenticate(
+  prevState: string | undefined,
+  formData: FormData,
+) {
+  try {
+    await signIn('credentials', formData);
+  } catch (error) {
+    if (error instanceof AuthError) {
+      console.log(error);
+      switch (error.type) {
+        case 'CredentialsSignin':
+          return 'Invalid credentials.';
+        default:
+          return 'Something went wrong.';
+      }
+    }
+    throw error;
+  }
 }
